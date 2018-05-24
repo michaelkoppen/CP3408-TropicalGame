192ac61bdf585b1f005e8d50adaec4c754068fca
michaelkoppen <mkoppen98@hotmail.com>
2018-05-25 04:59:18 +1000

    even more stuff?
    ree

---
+++ Assignment/Assets/stuffAddedInRecently1/PortalControlerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalControlerScript : MonoBehaviour {
+
+	//Initializing enemie
+	public GameObject enemy1;
+	public GameObject enemy2;
+	public GameObject enemy3;
+	public GameObject enemy4;
+	public GameObject enemy5;
+
+	public GameObject lvlPortal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		if (enemy1 == null && enemy2 == null && enemy3 == null && enemy4 == null && enemy5 == null)
+		{
+			lvlPortal.SetActive(true);
+		}
+	
+
+	}
+}
---
+++ Assignment/Assets/stuffAddedInRecently1/PortalScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour {
+
+	public GameObject player;
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player != null) {
+			transform.LookAt(player.transform);
+		}
+		
+	}
+}
