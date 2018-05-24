f6568a8f5329f6572eda416b1e29b41fe874675e
michaelkoppen <mkoppen98@hotmail.com>
2018-05-25 04:58:48 +1000

    Yeet
    Yoted

--- Assignment/Assets/AI Stuff/New AI Stuff/skeletonhealth.cs
+++ Assignment/Assets/AI Stuff/New AI Stuff/skeletonhealth.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//KYLE ADDED THIS
+using UnityEngine.SceneManagement;
+//END OF ADDING
+
 public class skeletonhealth : MonoBehaviour {
 
 	public float maxHealth = 100f;
@@ -10,29 +14,84 @@ public class skeletonhealth : MonoBehaviour {
 	public Slider healthbar;
 	Animator anim;
 
+	//KYLE ADDED THIS
+	[SerializeField] private string loadLevel;
+	public GameObject hurtScreen;
+	bool beingHurt;
+	//END OF ADDING
+
+
 
 	// Use this for initialization
 	void Start () {
+
 		currentHealth = maxHealth;
+
+		//KYLE ADDED THIS
+		beingHurt = false;
+		//END OF ADDING
+
+
+
 
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		//KYLE ADDED THIS
+		if (beingHurt && hurtScreen != null) {
+			hurtScreen.SetActive (true);
+		} else if(hurtScreen!= null) {hurtScreen.SetActive (false);
+		}
+		//END OF ADDING
+
+
+
 	}
 
 	public void healthUpdate(float damage) {
+		//KYLE ADDED THIS
+		StartCoroutine(FlashScreen());
+		//END OF ADDING
+
+
 		currentHealth += damage;
 		healthbar.value = currentHealth;
+
+
 		if (currentHealth <= 0) {
 			Death ();
 		}
 	}
 
 	public void Death() {
-		Destroy (gameObject, 1f);
-		anim.SetTrigger ("isDead");
 
+		//KYLE/MICHAEL ADDED THIS
+		if(gameObject.CompareTag("Player") && loadLevel != null) {
+			SceneManager.LoadScene (loadLevel);	
+		} else {
+			Destroy (gameObject, 1f);
+			anim.SetTrigger ("isDead");
+		}
+
+		//END OF ADDING
 	}
+
+	//ADDED BY KYLE
+	IEnumerator FlashScreen(){
+		if (hurtScreen != null) 
+		{
+			beingHurt = true;
+			float time = 0;
+			float hurtTime = 1f;
+			while (time < hurtTime) {
+
+				time += Time.deltaTime;
+				yield return null;
+			}
+			beingHurt = false;
+		}
+	}
+	//END OF ADDING
+
 }
---
+++ Assignment/Assets/WINSCRIPT.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WINSCRIPT : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+
+		if (other.CompareTag ("Player")) {
+			print ("YOU WIN");
+			Application.Quit();
+		}
+
+		print ("tehe");
+		if (other.CompareTag("boulder")) {
+			Destroy (other.gameObject);
+		}
+
+	}
+}
